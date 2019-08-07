fe75216acd9b6d4a12865f586b124678ae0c4bae
Matt Thornberry <mtberry16@hotmail.com>
2019-08-07 16:14:36 -0600

    Adding jQuery to button, and functionality to pull repos from GitHub

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 "use strict";
 
-const gitApiUrl = "https://api.github.com";
 // User enters GitHub handle
 // User clicks on search button
-function searchForHandle() {}
+function searchForHandle() {
+  $(".search-button").on("click", function(event) {
+    event.preventDefault();
+    let $searchHandle = $("#search-handle-input").val();
+    console.log($searchHandle);
+    fetchUserRepos($searchHandle);
+  });
+}
 
 // Search terms converted to endpoints
 function createEndPoints() {}
 
 // Fetch repositories based on search term endpoint
-function fetchUserRepos() {
+function fetchUserRepos(search) {
+  const gitApiUrl = "https://api.github.com/users/";
+  let fetchUrl = `${gitApiUrl}${search}/repos`;
+  console.log(fetchUrl);
+  fetch(fetchUrl).then(function(response) {
+    return response.json().then(function(responseJson) {
+      console.log(responseJson);
+    });
+  });
   // pass authentication tokens as o oAuth in header
   // catch errors
 }
@@ -18,4 +32,7 @@ function fetchUserRepos() {
 function displayReposInDom() {}
 
 // DOM Ready for manipulation
-$(displayReposInDom);
+$(document).ready(function() {
+  console.log("App ready, waiting for search terms!");
+  searchForHandle();
+});
