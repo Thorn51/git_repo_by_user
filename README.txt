5dc2e77da1984cbcbee75e7f358dc1cd05c87c39
Matt Thornberry <mtberry16@hotmail.com>
2019-08-07 13:32:31 -0600

    added functio stubs

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-This is an assignment completed for the Thinkful Full Stack Flex program in the Asynchronous Web Apps module. It's purpose is to utilize the GitHub API to search for and display repositories by a users handle. 
+This is an assignment completed for the Thinkful Full Stack Flex program in the Asynchronous Web Apps module. It's purpose is to utilize the GitHub API to search for and display repositories by a user's handle. 
 
 
 
---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const gitApiUrl = "https://api.github.com";
+// User enters GitHub handle
+// User clicks on search button
+function searchForHandle() {}
+
+// Search terms converted to endpoints
+function createEndPoints() {}
+
+// Fetch repositories based on search term endpoint
+function fetchUserRepos() {
+  // pass authentication tokens as o oAuth in header
+  // catch errors
+}
+
+// Manipulate DOM to display results
+function displayReposInDom() {}
+
+// DOM Ready for manipulation
+$(displayReposInDom);
