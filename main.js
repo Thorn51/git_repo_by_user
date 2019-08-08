76138dd8d87e5c5eef328d4a1c4e08aaf9cf2055
Matt Thornberry <mtberry16@hotmail.com>
2019-08-07 20:00:31 -0600

    added repos to DOM

--- main.js
+++ main.js
@@ -5,7 +5,9 @@
 function searchForHandle() {
   $(".search-button").on("click", function(event) {
     event.preventDefault();
-    let $searchHandle = $("#search-handle-input").val();
+    let $searchHandle = $("#search-handle-input")
+      .val()
+      .toLowerCase();
     console.log($searchHandle);
     fetchUserRepos($searchHandle);
   });
@@ -19,17 +21,36 @@ function fetchUserRepos(search) {
   const gitApiUrl = "https://api.github.com/users/";
   let fetchUrl = `${gitApiUrl}${search}/repos`;
   console.log(fetchUrl);
-  fetch(fetchUrl).then(function(response) {
-    return response.json().then(function(responseJson) {
-      console.log(responseJson);
+  fetch(fetchUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(responseJson) {
+      displayReposInDom(responseJson);
+    })
+    .catch(function(error) {
+      console.log(error);
     });
-  });
-  // pass authentication tokens as o oAuth in header
-  // catch errors
 }
 
 // Manipulate DOM to display results
-function displayReposInDom() {}
+function displayReposInDom(responseJson) {
+  // $('.repo-list')
+  let repoListHtml = "";
+  console.log(responseJson);
+  $(".repo-list-ul").append(
+    `<h3>GitHub Repositories by <a href="${responseJson[0].owner.html_url}"}>${
+      responseJson[0].owner.login
+    }</a></h3>`
+  );
+  for (let i = 0; i < responseJson.length; i++) {
+    $(".repo-list-ul").append(
+      `<li><a href="${responseJson[i].html_url}">${
+        responseJson[i].name
+      }</a></li>`
+    );
+  }
+}
 
 // DOM Ready for manipulation
 $(document).ready(function() {
